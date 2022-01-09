f7b564079e1379c4e09e55fda6cad55f7dc4024d
Maroua Sfaxi <sfaxi.maroua@gmail.com>
2022-01-09 22:18:20 +0100

    update join component

--- client /src/components/Join.js
+++ client /src/components/Join.js
@@ -1,19 +1,16 @@
-import React, { useState,useContext } from 'react';
-import {ChatContext} from '../context/SharedContext'
+import React, { useContext } from 'react';
+import { ChatContext } from '../context/SharedContext'
 import { Link } from 'react-router-dom';
 
 export default function Join() {
-   const {state}=useContext(ChatContext);
+    const { state } = useContext(ChatContext);
     return (
-        <div>
         
-                {/* <div>
-                    <input type="text" placeholder="Room" onChange={(e)=>setRoom(e.target.value)} />
-                </div> */}
-                <Link to={`/chat?room=${state.rooms[0]}`} >
-                <button type="submit" class="btn btn-primary">Sign In</button>
-                </Link>
-       
+        <div>
+            <Link to={`/chat?room=${state.rooms[0]}`} >
+                <button type="submit" class="btn btn-primary">Join</button>
+            </Link>
+
         </div>
     )
 }
